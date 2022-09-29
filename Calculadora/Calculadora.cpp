d5d9f5c1bd55eb6c4704544b3aa8531b4932334d
DQ <101306120+KinteroDQ@users.noreply.github.com>
2022-09-28 22:05:07 -0500

    Update Calculadora.cpp

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -1,2 +1,73 @@
 
-biububybu
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b, resultado, op;// estas son las variables a ocupar
+
+
+
+    printf( " $ ./ Calculadora ");
+    printf( " Este programa hace operaciones basicas entre dos numeros.");
+    printf( " Primer numero: 5 ");
+    printf( " Operacion: 1. + suma 2. - resta  3. * multiplicasion  4. / division 5. modulo  ");
+    printf( " Segundo numero: 8 ");
+
+    printf(" QUE OPERACION VAS A REALIZAR ");
+
+    scanf("%d", &op);
+    switch (op)
+    {
+        case 1:
+            printf("Ingrse el numero");
+            scanf("%d", &a);
+            printf("Ingrse el numero");
+            scanf("%d", &b);
+            resultado=a+b;
+            printf( " el resultado es=%d ", resultado);
+            break;
+
+        case 2:
+            printf("Ingrse el numero");
+            scanf("%d", &a);
+            printf("Ingrse el numero");
+            scanf("%d", &b);
+
+            resultado=a - b;
+            printf( " el resultado es=%d ", resultado);
+            break;
+
+        case 3:
+            printf("Ingrse el numero");
+            scanf("%d", &a);
+            printf("Ingrse el numero");
+            scanf("%d", &b);
+            resultado=a * b;
+            printf( " \" el resultado es=%d \" ", resultado);
+            break;
+        case 4:
+            printf("Ingrse el numero");
+            scanf("%d", &a);
+            printf("Ingrse el numero");
+
+            scanf("%d", &b);
+            resultado=a / b;
+            printf( " el resultado es=%d ", resultado);
+            break;
+
+
+        case 5:
+            printf("Ingrse el numero");
+            scanf("%d", &a);
+            printf("Ingrse el numero");
+            scanf("%d", &b);
+
+            resultado=a % b;
+            printf( " el resultado es=%d ", resultado);
+            break;
+    }
+    
+    system("loading");
+}
