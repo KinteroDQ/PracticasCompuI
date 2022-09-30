dcf0805c828173dc6ebdcd97bf8131da0f7703b6
DQ <101306120+KinteroDQ@users.noreply.github.com>
2022-09-29 22:28:48 -0500

    Update Calculadora.cpp

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -6,68 +6,69 @@ using namespace std;
 int main() {
 
     int a, b, resultado, op;// estas son las variables a ocupar
+    float resultadoDivicion ;
 
 
+        printf( " $ ./ Calculadora ");
+        printf( " Este programa hace operaciones basicas entre dos numeros.");
+        printf( " Primer numero: 5 ");
+        printf( " Operacion: 1. + suma 2. - resta  3. * multiplicasion  4. / division 5. modulo  ");
+        printf( " Segundo numero: 8 ");
 
-    printf( " $ ./ Calculadora ");
-    printf( " Este programa hace operaciones basicas entre dos numeros.");
-    printf( " Primer numero: 5 ");
-    printf( " /nOperacion: 1. + suma 2. - resta  3. * multiplicasion  4. / division 5. modulo  ");
-    printf( " Segundo numero: 8 ");
+        printf(" QUE OPERACION VAS A REALIZAR ");
 
-    printf(" QUE OPERACION VAS A REALIZAR ");
+        scanf("%d", &op);
+        switch (op)
+        {
+            case 1:
+                printf("Ingrese el numero");
+                scanf("%d", &a);
+                printf("Ingrese el numero");
+                scanf("%d", &b);
+                resultado=a+b;
+                printf( " el resultado es=%d ", resultado);
+                break;
 
-    scanf("%d", &op);
-    switch (op)
-    {
-        case 1:
-            printf("Ingrse el numero ");
-            scanf("%d", &a);
-            printf("/nIngrse el numero ");
-            scanf("%d", &b);
-            resultado=a+b;
-            printf( "/nel resultado es=%d ", resultado);
-            break;
+            case 2:
+                printf("Ingrese el numero");
+                scanf("%d", &a);
+                printf("Ingrese el numero");
+                scanf("%d", &b);
 
-        case 2:
-            printf("Ingrse el numero ");
-            scanf("%d", &a);
-            printf("/nIngrse el numero ");
-            scanf("%d", &b);
+                resultado=a - b;
+                printf( " el resultado es=%d ", resultado);
+                break;
 
-            resultado=a - b;
-            printf( "/nel resultado es=%d ", resultado);
-            break;
+            case 3:
+                printf("Ingrese el numero");
+                scanf("%d", &a);
+                printf("Ingrese el numero");
+                scanf("%d", &b);
+                resultado=a * b;
+                printf( " \" el resultado es=%d \" ", resultado);
+                break;
+            case 4:
+                printf("Ingrese el numero");
+                scanf("%d", &a);
+                printf("Ingrese el numero");
 
-        case 3:
-            printf("Ingrse el numero ");
-            scanf("%d", &a);
-            printf("/nIngrse el numero ");
-            scanf("%d", &b);
-            resultado=a * b;
-            printf( " /nel resultado es=%d ", resultado);
-            break;
-        case 4:
-            printf("Ingrse el numero ");
-            scanf("%d", &a);
-            printf("/nIngrse el numero ");
-
-            scanf("%d", &b);
-            resultado=a / b;
-            printf( " /nel resultado es=%d ", resultado);
-            break;
+                scanf("%d", &b);
+                resultadoDivicion=float(a) /float(b);
+                printf( " el resultado es=%f ", resultadoDivicion);
+                break;
 
 
-        case 5:
-            printf("Ingrse el numero ");
-            scanf("%d", &a);
-            printf("/nIngrse el numero ");
-            scanf("%d", &b);
+            case 5:
+                printf("Ingrese el numero");
+                scanf("%d", &a);
+                printf("Ingrese el numero");
+                scanf("%d", &b);
 
-            resultado=a % b;
-            printf( "/nel resultado es=%d ", resultado);
-            break;
-    }
-    
-    system("pause");
-}
+                resultado=a % b;
+                printf( " el resultado es=%d ", resultado);
+                break;
+        }
+        printf(" \n\n");
+        system("loading");
+        return 0;
+          }
