9ed5cc938a3cff99cac680da3360135c4ca2b1c2
DQ <101306120+KinteroDQ@users.noreply.github.com>
2022-10-16 23:20:56 -0500

    Update Calculadora.cpp

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-    int a=0, b=0, resultado; // estas son las variables tipo int a ocupar
+    float a=0, b=0, resultado; // estas son las variables tipo int a ocupar
     char op='+';             // estas son las variables tipo char a ocupar
 
     cout << "Este programa hace operaciones basicas entre dos numeros." << endl;
@@ -16,60 +16,41 @@ int main() {
     cout << "operacion: "; //ImprimimosElValorPedido
     cin >> op; //PedirUnNumero
     cout<< endl;
-
     while (op != '+' && op != '-' && op != '*' && op != '/' && op != '%')
     {
 
         cout << "operacion: ";
         cin >> op;
     }
-
-
     cout << "Segundo numero: " ; //ImprimimosElValorPedido
     cin >> b;//PedirUnNumero
-
-
-
     float resultadoDivicion,resultadoMultipocasion1;
-
     switch(op)
     {
-
         case '+':
-            resultado=a+b;
+            resultado= a+b;
             printf( " Resultado =%d ", resultado);
             break;
-
         case '-':
-
-            resultado=a - b;
+            resultado= a-b;
             printf( " Resultado =%d ", resultado);
             break;
-
         case '*':
-
-            resultadoMultipocasion1=  float (a) * float (b);
-
+            resultadoMultipocasion1=  a*b;
             printf( " Resultado =%f ", resultadoMultipocasion1);
             break;
-
         case '/':
-
             if (b!= 0) ;
             else printf( "NO ES POSIBLE LA OPERACION" );
-            resultadoDivicion=float(a) /float(b);
+            resultadoDivicion=a / b;
             printf( "  Resultado =%f ", resultadoDivicion);
-            float resultadoDivicion ( float (a), float (b) );
 
             break;
-
         case '%':
-
-            resultado=a % b;
+            resultado =int (a) % int (b);
             printf( " Resultado =%d ", resultado);
             break;
     }
     printf(" \n\n");
-
     return 0;
 }
