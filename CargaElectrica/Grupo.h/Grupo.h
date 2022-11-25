b4855f0e5986e690733906c8ceb4604a10052dfa
DQ <101306120+KinteroDQ@users.noreply.github.com>
2022-11-25 13:37:35 -0600

    Update Grupo.h

--- CargaElectrica/Grupo.h/Grupo.h
+++ CargaElectrica/Grupo.h/Grupo.h
@@ -1,1 +1,22 @@
+# include "Grupo.h"
+using namespace std;
+Grupo::Grupo(string nom, float p , int  c) {
+nombre=nom;
+cargaGrupo=c;
+PkGrupo=p;
+}
+Grupo::Grupo() {
+    nombre="eduardo";
+    cargaGrupo=0;
+    PkGrupo=0;
+}
 
+string Grupo::GetN() {
+    return nombre;
+}
+float Grupo::Getpk() {
+    return PkGrupo;
+}
+int Grupo::GetCar() {
+    return cargaGrupo;
+}
