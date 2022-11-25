0ad00fd8a201a1a501cc2f585b32047ab76056ba
DQ <101306120+KinteroDQ@users.noreply.github.com>
2022-11-25 13:38:44 -0600

    Update Grupo.cpp

--- CargaElectrica/Grupo.cpp/Grupo.cpp
+++ CargaElectrica/Grupo.cpp/Grupo.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <array>
+#include <string>
+#ifndef CARGAELECTRICA_GRUPO_H
+#define CARGAELECTRICA_GRUPO_H
+using namespace std;
+using namespace std;
 
+ class Grupo{
+ public:
+     Grupo(string ,float ,int );//Constructor
+     Grupo();
+     string GetN();
+     float Getpk();
+     int GetCar();
+
+
+ private:
+    string nombre;
+    int cargaGrupo;
+    float PkGrupo;
+};
+
+
+#endif //CARGAELECTRICA_GRUPO_H
